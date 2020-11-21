3e934db3e50e820669c2b65fec0d67e73f2883fc
Atsushi Kokubo <kokubo@hi-tech.ac.jp>
2020-11-21 10:22:25 +0900

    アプリケーションのコードを追加

---
+++ confusion-matrix-calculator.js
@@ -0,0 +1,92 @@
+/*global $ */
+
+class ConfusionMartixCalculator {
+    constructor() {
+        this.getOption();
+    }
+
+    // 小数点以下第1位で四捨五入
+    toPrecision1(num) {
+        return Math.round(num * 10.0) / 10;
+    }
+
+    // 混同行列を計算
+    calc() {
+        // フォームから値を取得
+        // 検査対象の総人数
+        var total = parseInt($("#total").val());
+        // 検査対象中の感染者数の予想
+        var infected = parseInt($("#infected").val());
+
+        // 混同行列を計算
+        var truePositive = infected * this.sensitivity / 100.0;
+        var falseNegative = infected * (100.0 - this.sensitivity) / 100.0;
+        var falsePositive = (total - infected) * (100.0 - this.specificity) / 100.0;
+        var trueNegative = (total - infected) * this.specificity / 100.0;
+
+        // 適合度を計算
+        var precision = truePositive / (truePositive + falsePositive);
+
+        // 混同行列の反映
+        $("#true-positive").html(this.toPrecision1(truePositive));
+        $("#false-negative").html(this.toPrecision1(falseNegative));
+        $("#false-positive").html(this.toPrecision1(falsePositive));
+        $("#true-negative").html(this.toPrecision1(trueNegative));
+
+        // 適合度の反映
+        $("#precision").html(this.toPrecision1(precision * 100));
+    }
+
+    // オプションを取得
+    getOption() {
+        // 検査の感度(罹っている人を検査した場合、陽性になる確率(%))
+        this.sensitivity = parseFloat($("#sensitivity").val());
+        // 検査の特異度(罹っていない人を検査した場合、陰性になる確率(%))
+        this.specificity = parseFloat($("#specificity").val());
+    }
+
+    // オプションを書き戻す
+    restoreOption() {
+        // 検査の感度(罹っている人を検査した場合、陽性になる確率(%))
+        $("#sensitivity").val(this.sensitivity);
+        // 検査の特異度(罹っていない人を検査した場合、陰性になる確率(%))
+        $("#specificity").val(this.specificity);
+    }
+}
+
+// メイン関数
+$(document).ready(function () {
+    const confusionMatrixCalculator = new ConfusionMartixCalculator();
+    // 計算を実行
+    confusionMatrixCalculator.calc();
+
+    // 再計算をクリックすると計算を実行
+    $("#recalc").on("click", function (event) {
+        // フォームを送信する挙動を止める
+        event.preventDefault();
+        // 混同行列を計算する
+        confusionMatrixCalculator.calc();
+    });
+
+    // モーダルを開いたとき、オプションの値を取得しておく
+    $('#optionModal').on('show.bs.modal', function () {
+        confusionMatrixCalculator.getOption();
+    });
+
+    // モーダルを閉じたとき、オプションの値を復元
+    $('#optionModal').on('hide.bs.modal', function () {
+        confusionMatrixCalculator.restoreOption();
+    });
+
+    // モーダルで適用をクリックすると計算を実行
+    $("#apply-option").on("click", function () {
+        // フォームを送信する挙動を止める
+        event.preventDefault();
+        // オプションの値を取得する
+        confusionMatrixCalculator.getOption();
+        // 混同行列を計算する
+        confusionMatrixCalculator.calc();
+        // モーダルを明示的に閉じる
+        $("#optionModal").modal("hide");
+    });
+});
